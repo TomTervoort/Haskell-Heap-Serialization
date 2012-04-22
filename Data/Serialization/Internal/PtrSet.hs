83219b4f551892148c158c122694c595fc88eb16
Tom Tervoort <tomervoort@gmail.com>
2012-04-22 13:07:06 +0200

    First working, although inefficient implementation of PtrSet.

--- Data/Serialization/Internal/PtrSet.hs
+++ Data/Serialization/Internal/PtrSet.hs
@@ -1,4 +1,5 @@
-module Data.Serialization.Internal.PtrSet (newPtrSet, ptrSetAdd, ptrSetMember) where
+{-# LANGUAGE RankNTypes, ExistentialQuantification, MagicHash #-}
+module Data.Serialization.Internal.PtrSet (PtrSet, PtrKey, newPtrSet, ptrSetAdd, ptrSetMember) where
 
 import Data.List
 import System.IO
@@ -7,21 +8,76 @@ import Data.List
 import Data.Maybe
 import Data.Char
 import Data.Bits
+import Control.Exception
 
 import Data.Typeable
 import Data.Data
 import Data.Generics
+import Data.IORef
+import Data.Array (Ix)
+
+import Foreign.StablePtr
+import System.Mem.Weak
+
+import System.Time
+
+import Unsafe.Coerce
+import GHC.Exts
+
+------------------
+
+data PtrBox a = PtrBox a
+data SBoxPtr = forall a. SBoxPtr (StablePtr (PtrBox a))
+
+data GCCheckPoint = forall a. GCCheckPoint (Weak a)
+
+----------------------
+
+data PtrSet = PtrSet [SBoxPtr]
+
+data PtrKey = PtrKey Int
+               deriving (Eq, Ord, Ix, Show)
+
+-------------------------
+
+gcCheckPoint :: IO GCCheckPoint
+gcCheckPoint = do key <- generateTemporaryPtr
+                  ptr <- mkWeakPtr key (Just $ return ())
+                  return $ GCCheckPoint ptr
+ where generateTemporaryPtr = getClockTime
+ 
+gcHasRun :: GCCheckPoint -> IO Bool
+gcHasRun (GCCheckPoint w) = fmap isNothing $ deRefWeak w
+ 
+ 
+
+ 
+unsafePtrCompare :: a -> a -> Bool
+unsafePtrCompare a b = reallyUnsafePtrEquality# a b /=# 0#
 
 
-data PtrSet = TODO
-
-data PtrKey = TODO2
+-- Tests whether a stable pointer points to the same value as another pointer. This operation is 
+-- atomic within GC runs.
+stablePtrCompare :: a -> SBoxPtr -> IO Bool
+stablePtrCompare x b@(SBoxPtr sp) = do cp <- gcCheckPoint
+                                       (PtrBox y) <- deRefStablePtr $ unsafeCoerce# sp
+                                       eq <- evaluate $ unsafePtrCompare x y
+                                       gc <- gcHasRun cp
+                                       if gc
+                                        then stablePtrCompare x b
+                                        else return eq                            
+                             
+-----------------------
 
 newPtrSet :: IO PtrSet
-newPtrSet = undefined
+newPtrSet = return $ PtrSet []
 
 ptrSetAdd :: a -> PtrSet -> IO PtrSet
-ptrSetAdd = undefined
+ptrSetAdd x (PtrSet ps) = do sp <- newStablePtr $ PtrBox x
+                             return $ PtrSet $ ps ++ [SBoxPtr sp]
 
 ptrSetMember :: a -> PtrSet -> IO (Maybe PtrKey)
-ptrSetMember = undefined
+ptrSetMember x (PtrSet ps) = do cmp <- forM ps (stablePtrCompare x)
+                                return $ fmap PtrKey $ findIndex id cmp
+
+
